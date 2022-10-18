bd7e863845cae60c40026163500a551b8457cf82
qnolan <qnolan@kentik.com>
2022-10-18 17:48:07 -0400

    dropbox download working

--- src/API/dropbox/main.go
+++ src/API/dropbox/main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"flag"
+	"io"
+	"os"
 	"types"
 
 	"github.com/dropbox/dropbox-sdk-go-unofficial/v6/dropbox"
-	"github.com/dropbox/dropbox-sdk-go-unofficial/v6/dropbox/users"
+	"github.com/dropbox/dropbox-sdk-go-unofficial/v6/dropbox/files"
 )
 
 type DropBoxClient struct {
 	types.APIClient
-	Srv  *users.Client
 	Name string
 }
 
-const TOKEN = "sl.BQiZltjI6NylGdMqa430mQZqyMV-dXhrLXMsVnoOox-Oa8wpMWhbeSfsJkohfwXcFKK7JGqpBzFP6ki_aVJC9K3Ni9Nt46mvDz-P901yGJ7JytPlmSey1nW8ngIsSfUYCw3e8OE"
+const TOKEN = "sl.BRb2-zP_JMm7Q1qEubUVcMCnLHUjztSN0qt4euXt6u1KLnLgSo2wpu6ahBEmqXQdAoeLCGrAbuJckvdQ8KRuyD-WxxmPQ0YyhJUNunpOYZKYiC114rG-WUNfPwDD4gQj1MgOkrZ6CSXq"
 
 func main() {
 
@@ -25,10 +26,34 @@ func main() {
 		Token:    TOKEN,
 		LogLevel: dropbox.LogInfo, // if needed, set the desired logging level. Default is off
 	}
-	dbx := users.New(config)
+	//dbx := users.New(config)
+	/*acc, err := dbx.GetCurrentAccount()
+	if err != nil {
+		log.Fatal(err)
+	}*/
 
-	api := types.APIClient{Client: &DropBoxClient{Srv: &dbx, Name: "Dropbox"}}
-	api.Serve(*debug)
+	arg := files.NewDownloadArg("/Main/midterm_links.txt")
+	dbx := files.New(config)
+	_, contents, err := dbx.Download(arg)
+	if err != nil {
+		panic(err)
+	}
+
+	f, err := os.Create("midterm_links.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	//progressBar := &ioprogress.Reader
+
+	if _, err = io.Copy(f, contents); err != nil {
+		panic(err)
+	}
+
+	if *debug {
+		api := types.APIClient{Client: &DropBoxClient{Name: "Dropbox"}}
+		api.Serve(*debug)
+	}
 }
 
 func (c *DropBoxClient) GetFiles(path string) ([]types.File, *types.APIError) {
