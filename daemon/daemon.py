69cf67d6d7dbb68212491bdd95c4835eab7d233a
Carson Botelho <carsonb19@vt.edu>
2022-11-07 00:59:16 +0000

    Untested sync in daemon

--- daemon/daemon.py
+++ daemon/daemon.py
@@ -96,6 +96,78 @@ def get_file_list(drive):
     return []
 
 
+def push_local_changes(local_files):
+    #Formats a JSON string containing the local files that need to be pushed out
+    filenames = "["
+    for cur_file in local_files:
+        filenames = filenames + '"' + path + cur_file + '", '
+    filenames = filenames[:-2] + "]"
+
+    #Loops through and writes local files to all remote drives
+    for drive in drives:
+        os.write(drive[3], bytes('{"command":"upload","path":"","files":' + filenames + '}\n', 'utf-8'))
+
+    for drive in drives:
+        os.read(drive, 300).decode('utf-8')
+
+
+def sync_remote_drives(remote_filelists, local_filelist):
+    #Parse through each remote filelist
+    #Download the files that were recently changed and add them to the list
+    downloaded = []
+    
+    time_breakpoint = time.time() - WAIT_TIME
+    i = 0
+
+    for file_list in remote_filelists:
+        temp_file_list = []
+        temp_file_string = "["
+        for cur_file in file_list:
+
+            #If a file has been recently modified remotely and not recently modified locally, we want to download it to be synced
+            if cur_file['Modified'] > time_breakpoint and cur_file['Name'] not in local_filelist:
+                print(cur_file['Name'])
+                temp_file_list.append(cur_file['Name'])
+                temp_file_string = temp_file_string + '"' + cur_file['Name'] + '", '
+        
+        #We have files recently modified so we need to download them
+        if len(temp_file_string) > 2:
+            temp_file_string = temp_file_string[:-2] + "]"
+            os.write(drives[i][3], bytes('{"command":"download","path":"' + path + '","files":' + temp_file_string + '}\n', 'utf-8'))
+            output = os.read(drives[i][4], 300).decode('utf-8')
+
+            #Parse through the error response and make note of all files successfully downloaded so they can later be pushed out
+            for name in temp_file_list:
+                if not output.__contains__(name):
+                    downloaded.append(name)
+                    print("Could not download " + name)
+            print(temp_file_list)
+        i += 1
+    
+    #At this point all files that were remotely modified have been downloaded and
+    #can be pushed out to all drives
+
+    #Nothing was changed remotely that we are able to sync, so just return
+    if len(downloaded) == 0:
+        return
+
+    #Format an upload string containing all files that were downloaded
+    upload_string = "["
+    for file_name in downloaded:
+        upload_string = upload_string + '"' + path + file_name + '", '
+    filenames = filenames[:-2] + "]"
+
+    #Upload this list of files to all remote drives
+    for drive in drives:
+        os.write(drive[3], bytes('{"command":"upload","path":"","files":' + upload_string + '}\n', 'utf-8'))
+
+    #For now, just discarding output so it doesn't mess up later reads
+    for drive in drives:
+        os.read(drive, 300).decode('utf-8')
+
+    
+
+
 def shutdown_api(drive_data):
     os.write(drive_data[3], bytes('{"command":"shutdown"}\n', 'utf-8'))
 
@@ -120,34 +192,36 @@ def main():
 
     while True:
 
-        delete_files()
-
         #Creates a list of filenames and the time they were last modified from the given directory
         #Currently only includes files modified after the most recent run 
         #Also excludes files starting with a '.' to avoid uploading the .delete and .lock files
-        files = [[x, os.path.getmtime(path + x)] for x in os.listdir(path) if os.path.getmtime(path + x) > time.time() - WAIT_TIME and x[0] != '.']
+        local_files = [x for x in os.listdir(path) if os.path.getmtime(path + x) > time.time() - WAIT_TIME and x[0] != '.']
 
         #Get list of all files from all drives
         remote_filelists = []
         for drive in drives:
-
             #Get the file list from this drive and append it to the overall list
             remote_filelists.append(get_file_list(drive))
+        
+        #Just for testing, will be removed eventually
+        #print(local_files)
+        #print(remote_filelists)
 
         #At this point we have a list of all local files which have been modified in 'files', 
         #and a list of files from each drive all stored in 'remote_filelists'
 
-        #Formats a string to hold a json list of the files that need to be uploaded
-        if files:
-            filenames = "["
-            for cur_file in files:
-                filenames += ('"' + path + cur_file[0] +'", ')
-            filenames = filenames[:-2] + "]"
+        #Sync the remote drives with each other first so that local changes have priority
+        if remote_filelists:
+            sync_remote_drives(remote_filelists, local_files)
 
-            #Loops through and writes local files to all remote drives
-            for drive in drives:
-                os.write(drive[3], bytes('{"command":"upload","path":"","files":' + filenames + '}\n', 'utf-8'))
 
+        #Push out all locally modified files
+        if local_files:
+            push_local_changes(local_files)
+        print("Done pushing")
+
+        #Parses the .delete file and deletes all files listed
+        delete_files()
 
         #Sleeps for the specified wait time before running again
         time.sleep(WAIT_TIME)
