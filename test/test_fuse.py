29646a03d3be4fe2c50330b6705242779eaa823b
Caleb Hicks <hcaleb@vt.edu>
2022-11-29 21:16:04 +0000

    Updated testing script to work with unified view, and tidied up go.mod for google_drive_client

--- test/test_fuse.py
+++ test/test_fuse.py
@@ -17,6 +17,8 @@ def main():
     time.sleep(2) # wait for fuse to start
     
     test_read() # run read tests
+    test_write()
+    test_delete()
 
     shutdown_fuse(fuse_proc) # unmount fuse, delete directory
 
@@ -33,7 +35,7 @@ def test_read():
     # Check that we can read a file that does not exist in the cache
     output("rm mnt/ramdisk/testread.txt") # remove file from cache. don't care about output
     run_test(
-        f"cat {FUSE_MOUNT_POINT}/Google_Drive/testread.txt", 
+        f"cat {FUSE_MOUNT_POINT}/testread.txt", 
         "this is a file for unit tests, please do not change it"
     )
 
@@ -42,6 +44,87 @@ def test_read():
         f"cat mnt/ramdisk/testread.txt", 
         "this is a file for unit tests, please do not change it"
     )
+
+def test_write():
+    print("\nTesting Write -------------\n")
+
+    # write to new file
+    run_test(
+        f"echo test > {FUSE_MOUNT_POINT}/test_write",
+        ""
+    )
+    # test file has been written to cache
+    run_test(
+        f"cat mnt/ramdisk/test_write",
+        "test\n"
+    )
+    # test file has been written to fuse mount point
+    run_test(
+        f"cat {FUSE_MOUNT_POINT}/test_write",
+        "test\n"
+    )
+    output("rm mnt/ramdisk/test_write")
+
+    # output(f"rm {FUSE_MOUNT_POINT}/test_write_file")
+
+def test_delete():
+    print("\nTesting Delete -------------\n")
+    """
+    1. create file
+        touch fusemtpt/test_delete
+    2. remove file
+        rm fusemtpt/test_delete
+    3. check that file is not in fuse, cache
+    4. check that file is added to .delete file
+    """
+    # delete delete.txt file in cache
+    output("rm mnt/ramdisk/delete.txt")
+    run_test(
+        f"echo test > {FUSE_MOUNT_POINT}/unit_test_delete",
+        ""
+    )
+    run_test(
+        f"rm {FUSE_MOUNT_POINT}/unit_test_delete",
+        ""
+    )
+
+    print("Checking file has been removed from fuse mount...")
+    file_list = output(f"ls {FUSE_MOUNT_POINT}")
+    if "unit_test_delete" not in file_list:
+        print("Success\n")
+    else:
+        print("File exists in mount point")
+
+    print("Checking file has been added to .delete.txt")
+    deleted_file_list = output(f"cat mnt/ramdisk/.delete.txt")
+    if "unit_test_delete" in deleted_file_list:
+        print("Success\n")
+    else:
+        print("File exists in mount point")
+
+def test_write_dir():
+    print("\nTesting new directory -------------\n")
+    print("clearing cache")
+    output("rm mnt/ramdisk/*")
+    run_test(
+        f"mkdir {FUSE_MOUNT_POINT}/test_new_dir",
+        ""
+    )
+    print("Checking folder has been added to fuse mount...")
+    file_list = output(f"ls {FUSE_MOUNT_POINT}")
+    if "test_new_dir" in file_list:
+        print("Success\n")
+    else:
+        print("File exists in mount point")
+    
+    print("Checking folder has been added to cache...")
+    file_list = output(f"ls mnt/ramdisk/")
+    if "test_new_dir" in file_list:
+        print("Success\n")
+    else:
+        print("File exists in mount point")
+
+
 
 
 # Helpers -------------------------------------------------------
